1ff7800ca73af9ec1b350e7096fb7088dd725100
acakebread <acakebread@gmail.com>
2025-07-23 11:48:45 +0100

    another test

--- Breaker/ServiceWorker.js
+++ Breaker/ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "pigbrain.io-Breaker-0.1.0";
 const contentToCache = [
     "Build/fc12e8940117d42879f5725b1d9e2506.loader.js",
     "Build/9af4535a4ca74e4e1da41d25d51620e1.framework.js.unityweb",
-    "Build/a8446ece0292f3320d50fc66c331e116.data.unityweb",
+    "Build/8a2ea5378bd0d67749f20d6acf59471c.data.unityweb",
     "Build/8a4aa82d5d59cc8567691c69f5290f00.wasm.unityweb",
     "TemplateData/style.css"
 
