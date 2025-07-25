99cfc0d8cb747cc62e6619e8d39d78d676791387
acakebread <acakebread@gmail.com>
2025-07-25 14:51:23 +0100

    removed analytics package

--- BreakerDebug/ServiceWorker.js
+++ BreakerDebug/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "pigbrain.io-Breaker-0.1.0";
 const contentToCache = [
-    "Build/d8b6e8f133173461cede067a9841650f.loader.js",
-    "Build/58e14f7bd143e67951ee67336a4647c7.framework.js.unityweb",
-    "Build/f6807dfe1cea68a4ad01c45c468a2c9f.data.unityweb",
-    "Build/b203db24413273e385e4da4dfdc0fe0e.wasm.unityweb",
+    "Build/14a41e81d6930ec885dbe9700adcfb3d.loader.js",
+    "Build/03fde88808007bb08b1ce2d6f9dbe670.framework.js.unityweb",
+    "Build/fe85c7b72e775953b95412d05f9867bc.data.unityweb",
+    "Build/8fc743bd155f50bdc3c307f16aa3c396.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
