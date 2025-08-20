659b3175b65a6d5df5c54002951832062cb477f7
acakebread <acakebread@gmail.com>
2025-08-21 00:36:53 +0100

    bigger buttons for mobile test

--- StudioCoreTest/ServiceWorker.js
+++ StudioCoreTest/ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "DefaultCompany-empty-0.1.10";
+const cacheName = "DefaultCompany-empty-0.1.11";
 const contentToCache = [
-    "Build/8bc9b56542361a2e7a198c11ab8b27b1.loader.js",
+    "Build/f1551dfe7e2c42a24bb896c2ead10ef7.loader.js",
     "Build/19b6b7c4adca116a63fb056538f8838b.framework.js.unityweb",
-    "Build/8fd164bdc44cd9039c68d1b1187b77bc.data.unityweb",
-    "Build/e0a5447347f63c1ab76517f0180a305a.wasm.unityweb",
+    "Build/2ce2385f0cdb8a1bd2ce0982aaf5abf0.data.unityweb",
+    "Build/59a82ace70de002a7d2dc0d337b06458.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
