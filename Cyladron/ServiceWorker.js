11618eec72720c88f44566a035a0f25d1abbbd04
Vitor F. Barbosa <vitor@crazygames.com>
2025-10-06 13:09:48 -0300

    Updated Cyladron with merge fix

--- Cyladron/ServiceWorker.js
+++ Cyladron/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "com.crazygames-Cyladron-0.1.28";
 const contentToCache = [
-    "Build/0281ddf98d343d3cf43973e682dab1b2.loader.js",
+    "Build/9c87547c9a24b5ea3d6cf9536e4f6a34.loader.js",
     "Build/6772b3b04a94fa0dad2fc1536bd5e579.framework.js.unityweb",
-    "Build/45ba13671182f4daafd3ed4a450d8a18.data.unityweb",
-    "Build/6f43f7117ef8ef4bebe208e7375c3f72.wasm.unityweb",
+    "Build/e6ac1c1e691ab2dc06e978ab4544e149.data.unityweb",
+    "Build/6bb9b5454f9718d5742dc1885b8678f2.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
