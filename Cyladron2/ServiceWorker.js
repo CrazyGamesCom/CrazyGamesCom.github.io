8f042a92cc5c8b5c9ba60f23d5b081303b5aa467
Vitor F. Barbosa <vitor@crazygames.com>
2025-10-29 12:49:21 -0300

    Trying a new folder for Cyladron

---
+++ Cyladron2/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "com.crazygames-Cyladron-0.1.37";
+const contentToCache = [
+    "Build/d0908631c775da7fc0941ad9e3cdbbd2.loader.js",
+    "Build/a32c6f344b74ae4238ec54029457b64d.framework.js.unityweb",
+    "Build/c975b2635401eae2349c241a2bb466d2.data.unityweb",
+    "Build/28c9a4bdb40c6ffdd301848e396ec3f8.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
